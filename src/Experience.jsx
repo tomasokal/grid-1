54f8dbedf65243dd097b51d57ceca5504d6f9f99
Tomas Okal <tomas.okal@gmail.com>
2023-07-31 21:59:13 -0500

    Add leva and refactor grid components.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,20 +1,50 @@
 
-import { useEffect } from 'react'
-import { Center, Stage } from '@react-three/drei'
+import { Center } from '@react-three/drei'
+import { useControls } from 'leva'
 
 import GridComponent from './GridComponent'
 
 export default function Experience()
 {
+
+    const { type, rows, columns, gap} = useControls(
+        { 
+            type: {
+                
+                options : {
+                    hexagon: "hexagon",
+                    square: "square"
+                }
+            }, 
+            rows: {
+                value: 7,
+                min: 1,
+                max: 21,
+                step: 1,
+            }, 
+            columns: {
+                value: 7,
+                min: 1,
+                max: 21,
+                step: 1,
+            },
+            gap: {
+                value: 0.25,
+                min: 0.01,
+                max: 1,
+                step: 0.01,
+            }
+        }
+    )
     
     return <>
 
         <Center>
             <GridComponent 
-                type="triangle"
-                gap={0.25}
-                row={11}
-                col={11}
+                type={type}
+                gap={gap}
+                row={rows}
+                col={columns}
             />
         </Center>
 
--- src/GridComponent.jsx
+++ src/GridComponent.jsx
@@ -1,89 +1,80 @@
-
+import { cloneElement, useEffect, useState } from 'react'
 import * as THREE from 'three'
-import { Geometry } from '@react-three/csg'
 
 export default function GridComponent(props)
 {
+
+    // Pull in and deconstruct props from above.
     const { type, gap, row, col } = props
 
+    // Check which type is selected
+    const hexagonType = type === "hexagon"
+    const squareType = type === "square"
+    const triangleType = type === "triangle"
+
+    // Set up state for neighbor grid cells
+    const [neighborCells, setNeighborCells] = useState({})
+
+    // Create function for interaction click based on row and col
+    const handleInteractionClick = (row, col) => {
+
+        const neighborCells = hexagonType ? getHexagonNeighbors(row, col) : getSquareNeighbors(row, col)
+        setNeighborCells(neighborCells)
+
+    }
+
+    // Grid is set up and rendered based on type, then pushed.
     const grid = []
 
-        if (type === "square")
-        {
-            for (let i = 0; i < row; i++)
-            {
-                for (let j = 0; j < col; j++)
-                {
-                    grid.push(
-                        <SquareCell
-                            // Position is set by the center of the cell.
-                            // Position has to shift by 1 unit in the x and z axis
-                            // There is a further shift by half the unit multiplied by the gap
-                            // This is because the center of the cell is at the center of the cell
-                            // Finally, there is a shift of the gap multiplied by the unit
-                            // This happens for both the x and z axis
-                            position={[
-                                i + i * 0.5 + i * gap,
-                                0,
-                                j + j * 0.5 + j * gap,
-                            ]}
-                        />
-                    )
-                }
-            }
+    // Helper items to calculate radius and inradius.
+    // This is needed because grid cells are actually regular polygons.
+    const radius = 1
+    const inradius = radius / 2
+    const a = 2 * inradius * Math.tan(Math.PI / 3)
+
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            grid.push(
+                <InteractionEffects
+                    type={type}
+                    row={i}
+                    col={j}
+                    neighborCells={neighborCells}
+                    onClick={handleInteractionClick}
+                >
+                    {hexagonType ? <HexagonCell i={i} j={j} gap={gap} /> : <SquareCell i={i} j={j} gap={gap} />}
+                </InteractionEffects>
+            )
         }
+    }
 
-        if (type === "hexagon")
-        {
-            for (let i = 0; i < row; i++)
-            {
-                for (let j = 0; j < col; j++)
-                {
-                    grid.push(
-                        <HexagonCell
-                            // Position is set by the center of the cell.
-                            // For the x axis, we multiply the unit by the square root of 3 and add the gap
-                            // We then multiply by i and add j % 2 * 0.5
-                            // This is because every other row is shifted by half the unit because of the rotation of the hexagon
-                            // For the z axis, we multiply the unit by 1.5 and add the gap
-                            position={[
-                                (i + j % 2 * 0.5) * (Math.sqrt(3) + gap),
-                                0,
-                                j * (1.5 + gap)
-                            ]}
-                        />
-                    )
-                }
-            }
-        }
+    // if (type === "triangle")
+    // {
 
-        if (type === "triangle")
-        {
+    //     const radius = 1
+    //     const inradius = radius / 2
+    //     const a = 2 * inradius * Math.tan(Math.PI / 3)
 
-            const radius = 1
-            const inradius = radius / 2
-            const a = 2 * inradius * Math.tan(Math.PI / 3)
+    //     console.log(a)
 
-            console.log(a)
-
-            for (let i = 0; i < row * 2; i++)
-            {
-                for (let j = 0; j < col; j++)
-                {
-                    grid.push(
-                        <TriangleCell
-                            position={[
-                                i * a * 0.5 + j * a * 0.5 + i * gap + j * gap,
-                                0, 
-                                // j
-                                j * gap + j + j * (a / Math.sqrt(3) / 3) + j * (a / Math.sqrt(3) / 6) + (i % 2) * ( (a / Math.sqrt(3) / 3 ) ) - ( (1 + i) % 2) * ( (a / Math.sqrt(3) / 6 ) )
-                            ]}
-                            rotation={[0, (i % 2) * Math.PI, 0]}
-                        />
-                    )
-                }
-            }
-        }
+    //     for (let i = 0; i < row * 2; i++)
+    //     {
+    //         for (let j = 0; j < col; j++)
+    //         {
+    //             grid.push(
+    //                 <TriangleCell
+    //                     position={[
+    //                         i * a * 0.5 + j * a * 0.5 + i * gap + j * gap,
+    //                         0, 
+    //                         // j
+    //                         j * gap + j + j * (a / Math.sqrt(3) / 3) + j * (a / Math.sqrt(3) / 6) + (i % 2) * ( (a / Math.sqrt(3) / 3 ) ) - ( (1 + i) % 2) * ( (a / Math.sqrt(3) / 6 ) )
+    //                     ]}
+    //                     rotation={[0, (i % 2) * Math.PI, 0]}
+    //                 />
+    //             )
+    //         }
+    //     }
+    // }
     
     return <>
 
@@ -98,13 +89,24 @@ export default function GridComponent(props)
 function HexagonCell(props)
 {
 
-    const { position } = props
+    const { i, j, gap, color } = props
+
+    // Position is set by the center of the cell.
+    // For the x axis, we multiply the unit by the square root of 3 and add the gap
+    // We then multiply by i and add j % 2 * 0.5
+    // This is because every other row is shifted by half the unit because of the rotation of the hexagon
+    // For the z axis, we multiply the unit by 1.5 and add the gap
+    const position=[
+        (i + j % 2 * 0.5) * (Math.sqrt(3) + gap),
+        0,
+        j * (1.5 + gap)
+    ]
 
     return <>
 
         <mesh position={position} >
             <cylinderGeometry args={[1, 1, 0.25, 6]} />
-            <meshBasicMaterial color="red" />
+            <meshBasicMaterial color={color} />
         </mesh>
 
     </>
@@ -114,13 +116,25 @@ function HexagonCell(props)
 function SquareCell(props)
 {
 
-    const { position } = props
+    const { i, j, gap, color } = props
+
+    // Position is set by the center of the cell.
+    // Position has to shift by 1 unit in the x and z axis
+    // There is a further shift by half the unit multiplied by the gap
+    // This is because the center of the cell is at the center of the cell
+    // Finally, there is a shift of the gap multiplied by the unit
+    // This happens for both the x and z axis
+    const position = [
+        i + i * 0.5 + i * gap,
+        0,
+        j + j * 0.5 + j * gap,
+    ]
 
     return <>
 
         <mesh position={position} rotation={[0, Math.PI / 4, 0]}>
             <cylinderGeometry args={[1, 1, 0.25, 4]}/>
-            <meshBasicMaterial color="red" />
+            <meshBasicMaterial color={color} />
         </mesh>
 
     </>
@@ -138,9 +152,82 @@ function TriangleCell(props)
 
         <mesh position={position} rotation={rotation}>
             <cylinderGeometry args={[1, 1, 0.25, 3]}/>
-            <meshBasicMaterial color="red" />
+            <meshBasicMaterial color={color} />
         </mesh>
 
     </>
 
 }
+
+function InteractionEffects({ type, row, col, neighborCells, onClick, children })
+{
+
+    const [hovered, setHovered] = useState(false)
+    const [neighbor, setNeighbor] = useState(false)
+  
+    // Define your hover logic here
+    const colorOnHover = hovered ? 'blue' : 'red'
+
+    const handleClick = () => {
+        onClick(row, col)
+    }
+
+    useEffect(() => {
+        setNeighbor(containsArray(neighborCells, [row, col]))
+    }, [neighborCells])
+
+    useEffect(() => {
+        setNeighbor(false)
+        setNeighbor(containsArray(neighborCells, [row, col]))
+    }, [type])
+
+  
+    return <>
+
+        <group
+            position={neighbor ? [0, 1, 0] : [0, 0, 0]}
+            onPointerEnter={() => setHovered(true)}
+            onPointerLeave={() => setHovered(false)}
+            onClick={handleClick}
+        >
+            {cloneElement(children, { color: colorOnHover })}
+        </group>
+    
+    </>
+}
+
+function getHexagonNeighbors(q, r) {
+
+    return [
+        r % 2 ? [q + 1, r] : [q + 1, r],
+        r % 2 ? [q, r - 1] : [q - 1, r - 1],
+        r % 2 ? [q + 1, r - 1] : [q, r - 1],
+        r % 2 ? [q - 1, r] : [q - 1, r],
+        r % 2 ? [q + 1, r + 1] : [q, r + 1],
+        r % 2 ? [q, r + 1] : [q - 1, r + 1]
+    ]
+
+}
+
+function getSquareNeighbors(q, r) {
+
+    return [
+        [q + 1, r],
+        [q - 1, r],
+        [q, r + 1],
+        [q, r - 1]
+    ]
+
+}
+
+function containsArray(objOfArrays, targetArray) {
+    for (const key in objOfArrays) {
+      if (Array.isArray(objOfArrays[key])) {
+        const array = objOfArrays[key];
+        if (JSON.stringify(array) === JSON.stringify(targetArray)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
